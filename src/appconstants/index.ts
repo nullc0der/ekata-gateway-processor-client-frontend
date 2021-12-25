350aa12fa01a18587fa1014a2f203537de54b682
Prasanta Kakati <prasantakakati1994@gmail.com>
2021-12-26 05:03:24 +0530

    supported currency changes based on env

--- src/appconstants/index.ts
+++ src/appconstants/index.ts
@@ -1,2 +1,5 @@
 // TODO: need to make it a API call, fetch this info from backend
-export const ALLOWED_CURRENCY = ['bitcoin', 'dogecoin', 'baza']
+export const ALLOWED_CURRENCY =
+    process.env.REACT_APP_SITE_TYPE === 'production'
+        ? ['bitcoin', 'dogecoin', 'monero', 'baza']
+        : ['bitcoin', 'dogecoin']
