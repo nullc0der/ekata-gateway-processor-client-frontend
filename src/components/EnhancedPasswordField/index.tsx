e490503ffe6115288ad805cb5715ccd9584dfd21
Prasanta Kakati <prasantakakati1994@gmail.com>
2022-02-18 18:18:55 +0530

    modified login page and added EnhancedPasswordField

---
+++ src/components/EnhancedPasswordField/index.tsx
@@ -0,0 +1,178 @@
+import React, { useEffect, useState } from 'react'
+import { zxcvbn, zxcvbnOptions } from '@zxcvbn-ts/core'
+import zxcvbnCommonPackage from '@zxcvbn-ts/language-common'
+import zxcvbnEnPackage from '@zxcvbn-ts/language-en'
+
+import {
+    Icon,
+    IconButton,
+    InputAdornment,
+    TextField,
+    LinearProgress,
+    linearProgressClasses,
+    Box,
+    Theme,
+    Typography,
+    Collapse,
+} from '@mui/material'
+import { TransitionGroup } from 'react-transition-group'
+
+const options = {
+    translations: zxcvbnEnPackage.translations,
+    graphs: zxcvbnCommonPackage.adjacencyGraphs,
+    dictionary: {
+        ...zxcvbnCommonPackage.dictionary,
+        ...zxcvbnEnPackage.dictionary,
+    },
+}
+
+zxcvbnOptions.setOptions(options)
+
+interface EnhancedPasswordFieldProps {
+    id: string
+    label: string
+    error: boolean
+    helperText: string
+    required?: boolean
+    fullWidth?: boolean
+    name?: string
+    showPasswordStrength: boolean
+    onChange: (id: string, value: string) => void
+}
+
+interface PasswordFeedback {
+    warning: string
+    suggestions?: string[]
+}
+
+const getPasswordStrengthBarColor = (strength: Number, theme: Theme) => {
+    if (strength === 0) {
+        return theme.palette.error.dark
+    }
+    if (strength === 1) {
+        return theme.palette.error.light
+    }
+    if (strength === 2) {
+        return theme.palette.warning.dark
+    }
+    if (strength === 3) {
+        return theme.palette.warning.light
+    }
+    if (strength === 4) {
+        return theme.palette.success.main
+    }
+    return theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800]
+}
+
+const EnhancedPasswordField = (props: EnhancedPasswordFieldProps) => {
+    const [value, setValue] = useState('')
+    const [showPassword, setShowPassword] = useState(false)
+    const [passwordScore, setPasswordScore] = useState(0)
+    const [passwordFeedback, setPasswordFeedback] = useState<PasswordFeedback>()
+
+    const onChangeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value)
+        props.onChange(event.target.id, event.target.value)
+    }
+
+    useEffect(() => {
+        if (value && props.showPasswordStrength) {
+            const { score, feedback } = zxcvbn(value)
+            setPasswordScore(score)
+            setPasswordFeedback(feedback)
+        }
+    }, [value, props.showPasswordStrength])
+
+    return (
+        <Box
+            sx={{
+                display: 'inline-flex',
+                flexDirection: 'column',
+                width: props.fullWidth ? '100%' : 'auto',
+            }}>
+            <TextField
+                margin="normal"
+                id={props.id}
+                label={props.label}
+                type={showPassword ? 'text' : 'password'}
+                required={props.required ?? false}
+                fullWidth={props.fullWidth ?? false}
+                name={props.name ?? ''}
+                value={value}
+                error={props.error}
+                helperText={props.helperText}
+                onChange={onChangeValue}
+                InputProps={{
+                    endAdornment: (
+                        <InputAdornment position="end">
+                            <IconButton
+                                edge="end"
+                                onClick={() => setShowPassword(!showPassword)}>
+                                <Icon>
+                                    {showPassword
+                                        ? 'visibility_off'
+                                        : 'visibility'}
+                                </Icon>
+                            </IconButton>
+                        </InputAdornment>
+                    ),
+                }}
+            />
+            {props.showPasswordStrength && (
+                <>
+                    <Typography variant="body2" mt={1}>
+                        Password Strength
+                    </Typography>
+                    <LinearProgress
+                        sx={(theme) => ({
+                            height: 10,
+                            borderRadius: 5,
+                            [`&.${linearProgressClasses.colorPrimary}`]: {
+                                backgroundColor:
+                                    theme.palette.grey[
+                                        theme.palette.mode === 'light'
+                                            ? 200
+                                            : 800
+                                    ],
+                            },
+                            [`& .${linearProgressClasses.bar}`]: {
+                                borderRadius: 5,
+                                backgroundColor: getPasswordStrengthBarColor(
+                                    passwordScore,
+                                    theme
+                                ),
+                            },
+                        })}
+                        variant="determinate"
+                        value={((passwordScore + 1) * 100) / 5}
+                    />
+                    <TransitionGroup>
+                        {passwordFeedback?.warning && (
+                            <Collapse>
+                                <Typography
+                                    variant="subtitle1"
+                                    color="grey.600"
+                                    component="p">
+                                    {passwordFeedback.warning}
+                                </Typography>
+                            </Collapse>
+                        )}
+                        {passwordFeedback?.suggestions &&
+                            passwordFeedback.suggestions.map((x, i) => (
+                                <Collapse key={i}>
+                                    <Typography
+                                        component="p"
+                                        variant="subtitle2"
+                                        color="grey.500">
+                                        {x}
+                                    </Typography>
+                                </Collapse>
+                            ))}
+                    </TransitionGroup>
+                </>
+            )}
+        </Box>
+    )
+}
+
+export default EnhancedPasswordField
--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -7,17 +7,14 @@ import get from 'lodash/get'
 
 import jwt_decode from 'jwt-decode'
 
-import Avatar from '@mui/material/Avatar'
-import Icon from '@mui/material/Icon'
 import Box from '@mui/material/Box'
-import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid'
 import Link from '@mui/material/Link'
-import Paper from '@mui/material/Paper'
 import Alert from '@mui/material/Alert'
+import { useMediaQuery, useTheme } from '@mui/material'
 
 import { useMatomo } from '@datapunt/matomo-tracker-react'
 
@@ -26,6 +23,11 @@ import { useAppDispatch, useAppSelector } from 'hooks/reduxHooks'
 import { authenticateUser } from 'store/authSlice'
 import { isTokenNotExpired } from 'utils/auth'
 import HelpTooltip from 'components/HelpTooltip'
+import Logo from 'assets/image/logo.svg'
+import LoginSVG from 'assets/image/login.svg'
+import EnhancedPasswordField from 'components/EnhancedPasswordField'
+
+// TODO: Add loader and check layout and check password strength properly and password strength show flag
 
 const Login = () => {
     const dispatch = useAppDispatch()
@@ -44,6 +46,8 @@ const Login = () => {
     })
     const [twoFactorCodeRequired, setTwoFactorCodeRequired] = useState(false)
     const [loginError, setLoginError] = useState('')
+    const theme = useTheme()
+    const isXS = useMediaQuery(theme.breakpoints.down('sm'))
 
     let navigateTo = location.state?.from?.pathname || '/projects'
 
@@ -51,10 +55,10 @@ const Login = () => {
         trackPageView({})
     }, [trackPageView])
 
-    const onChangeFormData = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const onChangeFormData = (id: string, value: string) => {
         setFormData((prevState) => ({
             ...prevState,
-            [event.target.id]: event.target.value,
+            [id]: value,
         }))
     }
 
@@ -139,116 +143,177 @@ const Login = () => {
     return auth.isAuthenticated && isTokenNotExpired() ? (
         <Navigate to={navigateTo} replace={true} />
     ) : (
-        <Container
-            maxWidth="xs"
-            sx={{ display: 'flex', alignItems: 'center', minHeight: '100vh' }}>
-            <Paper
+        <Box
+            sx={{
+                display: 'flex',
+                height: '100vh',
+            }}>
+            <Box
                 sx={{
                     display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flex: 70,
+                }}>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '40%',
+                    }}>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                        }}>
+                        <img
+                            src={Logo}
+                            alt="Logo"
+                            style={{ width: '64px', height: '64px' }}
+                        />
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'end',
+                            }}>
+                            <Typography
+                                component="h1"
+                                variant="h4"
+                                color="primary">
+                                Login
+                            </Typography>
+                            <Typography component="span" variant="subtitle2">
+                                Enter email and password below to continue
+                            </Typography>
+                        </Box>
+                    </Box>
+                    <Box
+                        component="form"
+                        noValidate
+                        onSubmit={handleSubmit}
+                        sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            value={formData.email}
+                            error={!!formError.email}
+                            helperText={formError.email}
+                            onChange={(
+                                event: React.ChangeEvent<HTMLInputElement>
+                            ) =>
+                                onChangeFormData(
+                                    event.target.id,
+                                    event.target.value
+                                )
+                            }
+                        />
+                        <EnhancedPasswordField
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            id="password"
+                            showPasswordStrength={false}
+                            error={!!formError.password}
+                            helperText={formError.password}
+                            onChange={(id: string, value: string) =>
+                                onChangeFormData(id, value)
+                            }
+                        />
+                        {!!twoFactorCodeRequired && (
+                            <>
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    name="twoFactor"
+                                    label="Two Factor Code"
+                                    id="twoFactorCode"
+                                    value={formData.twoFactorCode}
+                                    error={!!formError.twoFactorCode}
+                                    helperText={formError.twoFactorCode}
+                                    onChange={(
+                                        event: React.ChangeEvent<HTMLInputElement>
+                                    ) =>
+                                        onChangeFormData(
+                                            event.target.id,
+                                            event.target.value
+                                        )
+                                    }
+                                />
+                                <Box
+                                    sx={{
+                                        display: 'flex',
+                                        justifyContent: 'flex-end',
+                                        alignItems: 'center',
+                                    }}>
+                                    <Typography variant="subtitle2">
+                                        Lost access to authenticator device{' '}
+                                    </Typography>
+                                    <HelpTooltip title="Use one of your recovery code" />
+                                </Box>
+                            </>
+                        )}
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ my: 3 }}
+                            disableElevation>
+                            Sign In
+                        </Button>
+                        {!!loginError && (
+                            <Alert severity="error" sx={{ mb: 3 }}>
+                                {loginError}
+                            </Alert>
+                        )}
+                    </Box>
+                    <Grid container justifyContent="space-between">
+                        <Grid item>
+                            <Link
+                                component={RouterLink}
+                                to="/forgot-password"
+                                variant="body2">
+                                Forgot password?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link
+                                component={RouterLink}
+                                to="/register"
+                                variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+            <Box
+                sx={{
+                    flex: 30,
+                    display: isXS ? 'none' : 'flex',
                     flexDirection: 'column',
                     alignItems: 'center',
-                    padding: 4,
-                    backgroundImage: 'none',
-                }}
-                elevation={6}>
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                    <Icon>lock-outline</Icon>
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Login
+                    justifyContent: 'center',
+                    borderLeft: '1px solid #eee',
+                }}>
+                <img src={LoginSVG} style={{ width: '250px' }} alt="Login" />
+                <Typography
+                    component="span"
+                    variant="h3"
+                    color="primary"
+                    mt={1}>
+                    Hi, Welcome Back
                 </Typography>
-                <Box
-                    component="form"
-                    noValidate
-                    onSubmit={handleSubmit}
-                    sx={{ mt: 1 }}>
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        value={formData.email}
-                        error={!!formError.email}
-                        helperText={formError.email}
-                        onChange={onChangeFormData}
-                    />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                        value={formData.password}
-                        error={!!formError.password}
-                        helperText={formError.password}
-                        onChange={onChangeFormData}
-                    />
-                    {!!twoFactorCodeRequired && (
-                        <>
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="twoFactor"
-                                label="Two Factor Code"
-                                id="twoFactorCode"
-                                value={formData.twoFactorCode}
-                                error={!!formError.twoFactorCode}
-                                helperText={formError.twoFactorCode}
-                                onChange={onChangeFormData}
-                            />
-                            <Box
-                                sx={{
-                                    display: 'flex',
-                                    justifyContent: 'flex-end',
-                                    alignItems: 'center',
-                                }}>
-                                <Typography variant="subtitle2">
-                                    Lost access to authenticator device{' '}
-                                </Typography>
-                                <HelpTooltip title="Use one of your recovery code" />
-                            </Box>
-                        </>
-                    )}
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ my: 3 }}>
-                        Sign In
-                    </Button>
-                    {!!loginError && (
-                        <Alert severity="error" sx={{ mb: 3 }}>
-                            {loginError}
-                        </Alert>
-                    )}
-                </Box>
-                <Grid container justifyContent="space-between">
-                    <Grid item>
-                        <Link
-                            component={RouterLink}
-                            to="/forgot-password"
-                            variant="body2">
-                            Forgot password?
-                        </Link>
-                    </Grid>
-                    <Grid item>
-                        <Link
-                            component={RouterLink}
-                            to="/register"
-                            variant="body2">
-                            {"Don't have an account? Sign Up"}
-                        </Link>
-                    </Grid>
-                </Grid>
-            </Paper>
-        </Container>
+            </Box>
+        </Box>
     )
 }
 
