87522bd4699dcf8e0625356467306183eefe8678
Prasanta Kakati <prasantakakati1994@gmail.com>
2022-02-19 16:19:03 +0530

    Redesigned VerifyEmail page

--- src/pages/Auth/VerifyEmail.tsx
+++ src/pages/Auth/VerifyEmail.tsx
@@ -3,19 +3,19 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import { Link as RouterLink } from 'react-router-dom'
 
-import Avatar from '@mui/material/Avatar'
-import Icon from '@mui/material/Icon'
-import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
-import Paper from '@mui/material/Paper'
 import Link from '@mui/material/Link'
+import { Box, useTheme, useMediaQuery } from '@mui/material'
 
+import Logo from 'assets/image/logo.svg'
 import { verifyEmail } from 'api/auth'
 
 const VerifyEmail = () => {
     const params = useParams()
     const [verificationResult, setVerificationResult] = useState('')
     const [showLoginLink, setShowLoginLink] = useState(false)
+    const theme = useTheme()
+    const isSM = useMediaQuery(theme.breakpoints.down('md'))
 
     useEffect(() => {
         const token = params.token || ''
@@ -30,35 +30,48 @@ const VerifyEmail = () => {
     }, [params.token])
 
     return (
-        <Container
-            maxWidth="xs"
-            sx={{ display: 'flex', alignItems: 'center', minHeight: '100vh' }}>
-            <Paper
+        <Box
+            sx={{
+                display: 'flex',
+                height: '100vh',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+            <Box
                 sx={{
                     display: 'flex',
                     flexDirection: 'column',
-                    alignItems: 'center',
-                    padding: 4,
-                    backgroundImage: 'none',
-                }}
-                elevation={6}>
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                    <Icon>lock-outline</Icon>
-                </Avatar>
-                <Typography component="h5" sx={{ mt: 2 }}>
-                    {verificationResult}
-                </Typography>
-                {!!showLoginLink && (
-                    <Link
-                        component={RouterLink}
-                        to={'/login'}
-                        variant="body2"
-                        sx={{ mt: 2 }}>
-                        Sign in
-                    </Link>
-                )}
-            </Paper>
-        </Container>
+                    width: isSM ? '80%' : '40%',
+                }}>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}>
+                    <img
+                        src={Logo}
+                        alt="Logo"
+                        style={{ width: '64px', height: '64px' }}
+                    />
+                    <Typography component="h1" variant="h5" color="primary">
+                        Verify Email
+                    </Typography>
+                    <Typography component="h5" sx={{ mt: 2 }}>
+                        {verificationResult}
+                    </Typography>
+                    {!!showLoginLink && (
+                        <Link
+                            component={RouterLink}
+                            to={'/login'}
+                            variant="body2"
+                            sx={{ mt: 2 }}>
+                            Sign in
+                        </Link>
+                    )}
+                </Box>
+            </Box>
+        </Box>
     )
 }
 
